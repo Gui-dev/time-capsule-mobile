ef2568f094b5b2e652070ccf9ffdfd22cb918315
Gui Silva <adrianon10@gmail.com>
2023-05-27 04:16:04 -0300

    feat: add home page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import { ActivityIndicator, ImageBackground, Text, View } from 'react-native'
+import {
+  ActivityIndicator,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -7,9 +13,13 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import colors from 'tailwindcss/colors'
+import { styled } from 'nativewind'
 
 import blurBackground from './src/assets/bg-blur.png'
 import Logo from './src/assets/logo.svg'
+import Stripes from './src/assets/stripes.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [isLoadedFonts] = useFonts({
@@ -29,14 +39,37 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBackground}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{
         position: 'absolute',
         left: '-100%',
       }}
     >
-      <Text className="font-alt text-5xl text-gray-50">Hello World!</Text>
-      <Logo height={20} width={210} />
+      <StyledStripes height={844} width={8} className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo height={20} width={210} />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            CADASTRAR LEMBRANÇA
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💚 by Gui Silva
+      </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
